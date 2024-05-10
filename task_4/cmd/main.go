a034d0bbbb770a1ccbb87212d90d5b16e0a3a3fe
Limes Braum <limesbra@tr-b9.nsk.21-school.ru>
2024-05-10 16:29:41 +0700

    task_4

---
+++ task_4/cmd/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+)
+
+// worker - функция, которая читает данные из канала и выводит их в stdout.
+func worker(ch chan string) {
+	for {
+		msg, ok := <-ch
+		// если канал закрыт, то выход из цикла
+		if !ok {
+			return
+		}
+		fmt.Println(msg)
+		time.Sleep(time.Second)
+	}
+}
+
+func main() {
+
+	N, err := strconv.Atoi(os.Args[1]) // количество воркеров которое задаем при старте программы
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	sig := make(chan os.Signal, 1)     // sig - создание канала для сигналов операционной системы
+	signal.Notify(sig, syscall.SIGINT) // signal.Notify - регистрация канала для получения сигнала SIGINT
+
+	ctx, cancel := context.WithCancel(context.Background()) // ctx - создание контекста с возможностью отмены операций
+
+	data := make(chan string)
+
+	// запуск N потоков worker
+	for i := 0; i < N; i++ {
+		go worker(data)
+	}
+
+	// запуск горутины, которая постоянно записывает в канал данные
+	go func() {
+		for {
+			select {
+			// Ожидание сигнала завершения контекста ctx
+			case <-ctx.Done():
+				// Закрытие канала данных и завершение функции
+				close(data)
+				return
+			default:
+				data <- time.Now().String()
+				time.Sleep(time.Second)
+			}
+
+		}
+	}()
+
+	fmt.Println("\nполученный сигнал -", <-sig, "\nПрограмма завершена") // ожидание сигнала
+	cancel()                                                             // отмена операций
+}
